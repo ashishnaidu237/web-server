6398e7ce23ac7a2e527470406c5ad1a155b8144a
ashishnaidu237 <ashishnaidu2@gmail.com>
2016-03-01 17:37:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express= require('express');
 var app= express();
+var http=require('http');
+var cassandra= require('cassandra-driver');
 var PORT=process.env.PORT || 3000;
+var client = new cassandra.Client({contactPoints:['127.0.0.1']});
+client.connect(function(err,result)
+{
+	console.log('app:  cassandra connected');
+})
+var getAllSubscribers="select * from people.subscribers  ";
 
 var middleware= require('./middleware.js');
+http.createServer(function(req,res){
+
+	res.writeHead(200,{'Content-Type':'text/plain'});
+	client.execute(getAllSubscribers,[],function(err,result)
+{
+	
+//console.log(result);
+	res.end(JSON.stringify({result}));
+});
 
 app.use(middleware.logger);
 app.get('/about', middleware.requireAuthentication, function(req,res)
